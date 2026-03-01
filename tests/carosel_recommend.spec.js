5a5d44dc402c7ed497c935d689ed151bb176bd73
csb6663345 <bpluem011@gmail.com>
2026-03-02 00:06:10 +0700

    carosel_recommend add expect value

--- tests/carosel_recommend.spec.js
+++ tests/carosel_recommend.spec.js
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('carosel_recommend', async ({ page }) => {
   await page.goto('https://automationexercise.com/');
+
   await page.evaluate(async () => {
     await new Promise((resolve) => {
       let totalHeight = 0;
@@ -9,7 +10,7 @@ test('carosel_recommend', async ({ page }) => {
       const timer = setInterval(() => {
         window.scrollBy(0, distance);
         totalHeight += distance;
-        if (totalHeight >= document.body.scrollHeight - 1000) {
+        if (totalHeight >= document.body.scrollHeight - 1200) {
           clearInterval(timer);
           resolve();
         }
@@ -17,16 +18,16 @@ test('carosel_recommend', async ({ page }) => {
     });
   });
 
-  await page.waitForTimeout(2000);
+  await page.waitForTimeout(1000);
+  await expect(page.locator('#recommended-item-carousel').getByText('Stylish Dress')).toBeVisible(); //check carosel is default (stylist dress)
+
+
   await page.locator('.right.recommended-item-control').click();
   await page.waitForTimeout(1000);
-  await page.locator('.right.recommended-item-control').click();
-  await page.waitForTimeout(1000);
-  await page.locator('.right.recommended-item-control').click();
-  await page.waitForTimeout(1000);
+  await expect(page.locator('#recommended-item-carousel').getByText('Blue Top')).toBeVisible(); //check carosel move to blue top
+
+  // กดซ้าย 1 ครั้ง
   await page.locator('.left.recommended-item-control').click();
   await page.waitForTimeout(1000);
-  await page.locator('.left.recommended-item-control').click();
-  await page.waitForTimeout(1000);
-  await page.locator('.left.recommended-item-control').click();
+  await expect(page.locator('#recommended-item-carousel').getByText('Stylish Dress')).toBeVisible(); //check carosel back to stylish dress
 });
